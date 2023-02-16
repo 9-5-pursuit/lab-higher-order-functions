c58c1c7e572d55498f6f38123f3d90a124507e02
ShakeenaLesane <example@email.com>
2023-02-16 03:39:11 -0500

    completed assignment 	modified:   index.js

--- index.js
+++ index.js
@@ -13,10 +13,32 @@
  *  //> undefined
  */
 function find(array, callback) {
+  //will iterate through each element in the array
+
   for (let element of array) {
-    // Write your code here.
+    //registers the callback function to the current element
+
+    if (callback(element)) {
+      //will return the element if the callback function returns truthy value
+
+      return element;
+    }
   }
+  return undefined; //returns undefined if no elementin the array meets the conditions
 }
+const array1 = [1, 2, 3]; //created new array with values 1-3
+
+const callback1 = (element) => element > 1; //checks is the element is greater than one, returns returns "true" for elements 2 and 3.
+
+const result1 = find(array1, callback1); //array1 and callback1 are the arguements within the "find" function. The "find" function will iterate over every element in the array and then apply the "callback1" function to each element.
+//second element is 2 so returns "true" because 2 is greater than the value 1.
+
+const array2 = [1, 2, 3]; //creates array with the values 1-3
+
+const callback2 = (element) => element < 0; //checks if the element is less than zero, returns "false" for elements in array 2.
+
+const result2 = find(array2, callback2); //array2 and callback2 are the arguements within the "find" function. The "find" function will iterate over every element in the array and then apply the "callback2" function to each element.
+//none of the elements in array2 meet the condition of less than 0 so the callback2 function returns false and the output will be "undefined".
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -33,11 +55,16 @@ function find(array, callback) {
  *  //> []
  */
 function filter(array, callback) {
-  const result = [];
+  const result = []; //empty array holds filter elements
+
+  //loops through each element in the array
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      //if callback returns true, push element to the result array
+      result.push(element);
+    }
   }
-  return result;
+  return result; //returns filter result array
 }
 
 /**
@@ -56,10 +83,13 @@ function filter(array, callback) {
  */
 function map(array, callback) {
   const result = [];
+
   for (let element of array) {
-    // Write your code here.
+    //loop through each element in the array
+
+    result.push(callback(element)); //calls callback function and push the new value to the result array
   }
-  return result;
+  return result; //returns new result aaray
 }
 
 /**
@@ -77,7 +107,9 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
+    //loop continues until "i" is less than the length of the array. This function does not return a value
     // Write your code here.
+    callback(array[i], i, array); //3 arguements to callback function
   }
 }
 
