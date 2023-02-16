1b58e0a68dcd4726fe4877e19140a121699912e0
Jennifer <jenniferpeterson@pursuit.org>
2023-02-15 20:36:43 -0500

    function 2 complete

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if (callback(element)) {
+      result.push(element);
+    }
   }
   return result;
 }
