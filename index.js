7f613a9ee97329f539ea016726fdd1888f54edd6
Nigel Phillips <nigelphillips@pursuit.org>
2023-02-17 09:28:27 -0500

    annotations

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
  *  find([1, 2, 3], (element) => element < 0);
  *  //> undefined
  */
+//The "for" loop in the "find" function iterates through each element in the input array. For each iteration, the callback function is called with the current element as an argument. If the result of the callback function is "true", the current element is returned by the "find" function using the "return" statement, which immediately exits the loop and the function. this function can quickly find the first element in an array that meets a certain condition defined by the callback function, without the need to iterate through the entire array. This is useful when you want to find a specific element in an array without knowing its index or when you only need the first occurrence of such an element.
 function find(array, callback) {
   for (let element of array) {
     if (callback(element)) {
@@ -34,6 +35,7 @@ function find(array, callback) {
  *  filter([1, 2, 3], (element) => element < 0);
  *  //> []
  */
+//the "filter" function creates a new array that contains only those elements of the input array based on the test defined by the callback function.
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
@@ -57,6 +59,7 @@ function filter(array, callback) {
  *  map([], (element) => element < 0);
  *  //> []
  */
+//The purpose of this function is to transform each element in an array in a consistent way and return a new array with the transformed elements. This is useful when you want to create a new array based on an existing array without changing the original one.
 function map(array, callback) {
   const result = [];
   for (let element of array) {
@@ -79,6 +82,7 @@ function map(array, callback) {
  *  //> 20 1 3
  *  //> 30 2 3
  */
+// This function takes in two parameters for the purpose of iteration of all elements with in the array witout the need to create a new for loop via callback method.
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     callback(array[i],i,array)
